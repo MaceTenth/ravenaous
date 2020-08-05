d3552d887d838b2a5065743e81299c971b30211c
MaceTenth <shimon83@gmail.com>
2020-08-05 20:28:25 +0300

    Updates

---
+++ precache-manifest.f8a871147e7068cbbfb2006e6bdd898c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db74b1e149f7240f5546d7af4d66959",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caccd4f1b0c77341bba4",
+    "url": "/static/css/main.f064d655.chunk.css"
+  },
+  {
+    "revision": "9b79d8bf30f471e73407",
+    "url": "/static/js/2.47692abc.chunk.js"
+  },
+  {
+    "revision": "caccd4f1b0c77341bba4",
+    "url": "/static/js/main.8140380d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
